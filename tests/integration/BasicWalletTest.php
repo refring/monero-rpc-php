43abbab9ce6961352251aaa7f25235f6a3d3b4df
refactor-ring <refactor_ring0@proton.me>
2023-09-11 21:16:05 +0200

    Added some tests to BasicWalletTest

--- tests/integration/BasicWalletTest.php
+++ tests/integration/BasicWalletTest.php
@@ -3,25 +3,64 @@
 namespace RefRing\MoneroRpcPhp\Tests\integration;
 
 use PHPUnit\Framework\TestCase;
-use RefRing\MoneroRpcPhp\Exception\AddressNotInWalletException;
+use RefRing\MoneroRpcPhp\Builder;
 use RefRing\MoneroRpcPhp\Exception\HttpApiException;
-use RefRing\MoneroRpcPhp\Model\QueryKeyType;
+use RefRing\MoneroRpcPhp\Exception\InvalidLanguageException;
+use RefRing\MoneroRpcPhp\Exception\WalletExistsException;
+use RefRing\MoneroRpcPhp\Tests\TestHelper;
+use RefRing\MoneroRpcPhp\WalletRpcClient;
 
 class BasicWalletTest extends TestCase
 {
-    //    public function testQueryKey()
-    //    {
-    //        $httpClient = new \GuzzleHttp\Client();
-    //        $client = new \RefRing\MoneroRpcPhp\WalletRpcClient($httpClient, 'http://127.0.0.1:18083/json_rpc');
-    //        $this->assertSame('4b5299030c378545891a603d7e965a8b0b236bd5cd81484721591027e96f270a', $client->queryKey(QueryKeyType::VIEW_KEY)->key);
-    //    }
+    private static WalletRpcClient $rpcClient;
 
-    public function testGetAddressIndexException()
+    public static function setUpBeforeClass(): void
     {
-        $httpClient = new \GuzzleHttp\Client();
-        $client = new \RefRing\MoneroRpcPhp\WalletRpcClient($httpClient, 'http://127.0.0.1:18083/json_rpc');
-        //        $this->expectException(AddressNotInWalletException::class);
+        self::$rpcClient = (new Builder(TestHelper::WALLET_RPC_URL))
+            ->buildWalletClient();
+    }
+
+    public function testGetVersion(): void
+    {
+        $version = self::$rpcClient->getVersion();
+        $this->assertGreaterThan(0, $version->version);
+    }
+
+    public function testCreateWallet(): void
+    {
+        $this->expectNotToPerformAssertions();
+        self::$rpcClient->createWallet(TestHelper::getRandomWalletName(), null, 'English');
+    }
+
+    public function testCreateWalletWithEmptyPassword(): void
+    {
+        $this->expectNotToPerformAssertions();
+        self::$rpcClient->createWallet(TestHelper::getRandomWalletName(), '', 'English');
+    }
+
+    public function testCreateWalletWithPassword(): void
+    {
+        $this->expectNotToPerformAssertions();
+        self::$rpcClient->createWallet(TestHelper::getRandomWalletName(), TestHelper::WALLET_PWD_1, 'English');
+    }
+
+    public function testCreateWalletErrorAlreadyExists(): void
+    {
+        $this->expectException(WalletExistsException::class);
+        $walletName = TestHelper::getRandomWalletName();
+        self::$rpcClient->createWallet($walletName, null, 'English');
+        self::$rpcClient->createWallet($walletName, null, 'English');
+    }
+
+    public function testCreateWalletErrorInvalidLanguage(): void
+    {
+        $this->expectException(InvalidLanguageException::class);
+        self::$rpcClient->createWallet(TestHelper::getRandomWalletName(), null, 'Barolo');
+    }
+
+    public function testGetAddressIndexException(): void
+    {
         $this->expectException(HttpApiException::class);
-        $client->getAddressIndex('7BnERTpvL5MbCLtj5n9No7J5oE5hHiB3tVCK5cjSvCsYWD2WRJLFuWeKTLiXo5QJqt2ZwUaLy2Vh1Ad51K7FNgqcHgjW85o');
+        self::$rpcClient->getAddressIndex('7BnERTpvL5MbCLtj5n9No7J5oE5hHiB3tVCK5cjSvCsYWD2WRJLFuWeKTLiXo5QJqt2ZwUaLy2Vh1Ad51K7FNgqcHgjW85o');
     }
 }
