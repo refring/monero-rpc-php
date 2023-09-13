607b36170e44c55621b54f2552a694186812e8ad
refactor-ring <refactor_ring0@proton.me>
2023-09-13 14:33:58 +0200

    Added a test for connection failure

--- tests/integration/RpcAuthenticationTest.php
+++ tests/integration/RpcAuthenticationTest.php
@@ -3,14 +3,26 @@
 namespace RefRing\MoneroRpcPhp\Tests\integration;
 
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientExceptionInterface;
 use RefRing\MoneroRpcPhp\Builder;
 use RefRing\MoneroRpcPhp\Exception\AuthenticationException;
+use Symfony\Component\HttpClient\HttpClient;
 
 class RpcAuthenticationTest extends TestCase
 {
     public const WALLET_RPC_URL = 'http://127.0.0.1:18084/json_rpc';
     public const DAEMON_RPC_URL = 'http://127.0.0.1:18085/json_rpc';
-    public function testWalletAuthFailure()
+
+    public function testConnectionError(): void
+    {
+        $client = (new Builder('http://127.0.0.1:'.$this->findFreePort()))
+            ->buildWalletClient();
+
+        // @TODO Should this exception be wrapped ?
+        $this->expectException(ClientExceptionInterface::class);
+        $client->getVersion();
+    }
+    public function testWalletAuthFailure(): void
     {
         $client = (new Builder(self::WALLET_RPC_URL))
             ->buildWalletClient();
@@ -19,7 +31,7 @@ class RpcAuthenticationTest extends TestCase
         $client->getVersion();
     }
 
-    public function testWalletAuth()
+    public function testWalletAuth(): void
     {
         $client = (new Builder(self::WALLET_RPC_URL))
             ->withAuthentication('foo', 'bar')
@@ -28,7 +40,7 @@ class RpcAuthenticationTest extends TestCase
         $this->assertGreaterThan(0, $client->getVersion()->version);
     }
 
-    public function testDaemonAuthFailure()
+    public function testDaemonAuthFailure(): void
     {
         $client = (new Builder(self::DAEMON_RPC_URL))
             ->buildDaemonClient();
@@ -37,12 +49,27 @@ class RpcAuthenticationTest extends TestCase
         $client->getVersion();
     }
 
-    public function testDaemonAuth()
+    public function testDaemonAuth(): void
     {
         $client = (new Builder(self::DAEMON_RPC_URL))
             ->withAuthentication('foo', 'bar')
             ->buildDaemonClient();
 
         $this->assertGreaterThan(0, $client->getVersion()->version);
+    }
+
+    /**
+     * Find a free port on the system
+     */
+    private function findFreePort(): int
+    {
+        $socket = socket_create_listen(0);
+        if ($socket === false) {
+            throw new \Exception("Could not create open socket.");
+        }
+        socket_getsockname($socket, $addr, $port);
+        socket_close($socket);
+
+        return $port;
     }
 }
