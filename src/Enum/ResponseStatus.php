fa86b2bc5daa0b7074e849d9e741c798ed03f6c8
refactor-ring <refactor_ring0@proton.me>
2023-10-01 22:45:41 +0200

    Added ResponseStatus::FAILED

--- src/Enum/ResponseStatus.php
+++ src/Enum/ResponseStatus.php
@@ -8,6 +8,7 @@ enum ResponseStatus: string
 {
     case OK = 'OK';
     case BUSY = 'BUSY';
+    case FAILED = 'Failed';
     case NOT_MINING = 'NOT MINING';
     case PAYMENT_REQUIRED = 'PAYMENT REQUIRED';
 }
