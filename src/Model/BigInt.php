773066016a0fb67b57fd66b847c4432570b797b1
refactor-ring <refactor_ring0@proton.me>
2023-10-04 14:29:01 +0200

    Added BigInt class

---
+++ src/Model/BigInt.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace RefRing\MoneroRpcPhp\Model;
+
+use Square\Pjson\JsonDataSerializable;
+
+class BigInt implements JsonDataSerializable
+{
+    public function __construct(
+        protected string|int $value,
+    ) {
+    }
+
+    public static function fromJsonData($jd, array|string $path = []) : static
+    {
+        return new BigInt($jd);
+    }
+
+    public function toJsonData()
+    {
+        return $this->getValue();
+    }
+
+    public function getValue(): string
+    {
+        return $this->value;
+    }
+}
